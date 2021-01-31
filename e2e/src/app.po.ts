3b0895ac22409cb44eb2d891b5b69de6f2de773c
chebandoneon <jordanromaidis@gmail.com>
2021-01-31 20:48:28 +0200

    Fix broken tests

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -4,8 +4,7 @@ export class AppPage {
   async navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl)
   }
-
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText()
+  getTitleText(): Promise<string> {
+    return element(by.css('app-root mat-toolbar h1')).getText() as Promise<string>
   }
 }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,13 +22,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance
     expect(app.title).toEqual('lemon-mart')
   })
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.nativeElement
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'lemon-mart app is running!'
-    )
-  })
 })
