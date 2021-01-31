52955ac51abfa1a3d22e176446fe0bc761c92b04
chebandoneon <jordanromaidis@gmail.com>
2021-01-31 21:20:41 +0200

    Add common testing module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing'
+import { MediaObserver } from '@angular/flex-layout'
+import { MatIconRegistry } from '@angular/material/icon'
+import { DomSanitizer } from '@angular/platform-browser'
 import { RouterTestingModule } from '@angular/router/testing'
 
 import { AppComponent } from './app.component'
+import {
+  DomSanitizerFake,
+  MatIconRegistryFake,
+  MediaObserverFake,
+  commonTestingModules,
+} from './common/common.testing'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [commonTestingModules],
+      providers: [
+        { provide: MediaObserver, useClass: MediaObserverFake },
+        { provide: MatIconRegistry, useClass: MatIconRegistryFake },
+        { provide: DomSanitizer, useClass: DomSanitizerFake },
+      ],
       declarations: [AppComponent],
     }).compileComponents()
   })
--- src/app/user/profile/profile.component.spec.ts
+++ src/app/user/profile/profile.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { commonTestingModules } from 'src/app/common/common.testing'
 
 import { ProfileComponent } from './profile.component'
 
@@ -9,6 +10,7 @@ describe('ProfileComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProfileComponent],
+      imports: [commonTestingModules],
     }).compileComponents()
   })
 
